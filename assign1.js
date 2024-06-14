ece481498453bce5009a3ce7e5240ac899df9a45
Ikennaezewudo <philipkephilip@gmail.com>
2024-06-14 20:18:21 +0100

    Add files via upload

--- assign1.js
+++ assign1.js
@@ -1,6 +1,20 @@
+let username = "ikenna";
+let password = "philip";
+
+if(username === "ikenna") {
+    console.log("Username exist");
+} else {
+    console.log("Username does not exist");
+}
+
+if(password === "phili") {
+    console.log("password matches");
+} else {
+    console.log("password does not match");
+}
 
 const Names = ['Ebuka', 'Okay', 'Mike', 'Sunday', 'Chukwuma'];
 
-for(let i = 0; i < Names.lenght; i++){
+for(let i = 0; i < Names.length; i++) {
     console.log(Names[i]);
 }
